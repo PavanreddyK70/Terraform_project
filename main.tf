1b2757d567c76ec4e64df6842ee8e3f2408df5a8
PavanreddyK70 <pavanreddykodathala15@gmail.com>
2025-08-28 18:18:19 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "tf_vpc" {
 # 2. Create Subnet
 resource "aws_subnet" "tf_subnet" {
   vpc_id                  = aws_vpc.tf_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.2.0/24"
   availability_zone       = "${var.aws_region}a"
   map_public_ip_on_launch = true
   tags = { Name = "TF-Subnet" }
